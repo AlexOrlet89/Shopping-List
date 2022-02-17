9b34f6a8c3776f03ccf57beca07ecc87e713a6c7
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-17 15:36:51 -0800

    added event listener validated inputs

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -7,3 +7,12 @@ const logoutButton = document.getElementById('logout');
 logoutButton.addEventListener('click', () => {
     logout();
 });
+
+const create = document.getElementById('create');
+const quantity = document.getElementById('quantity');
+const item = document.getElementById('item');
+
+create.addEventListener('submit', async (e)=> {
+    e.preventDefault();
+    console.log(quantity.value, item.value);
+});
