eccb66d4d8b33a1749c39c1c819f89a8435bbeba
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-18 13:28:47 -0800

    list renders on window load, next step createlist

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,10 +1,19 @@
 import { checkAuth, logout, insertRow, fetchItems, deleteList } from '../fetch-utils.js';
 import { renderItem } from '../render-utils.js';
 
+const create = document.getElementById('create');
+const quantity = document.getElementById('quantity');
+const item = document.getElementById('item');
+const shoppingList = document.getElementById('shoppingList');
+
 checkAuth();
 window.addEventListener('load', async ()=> {
     const items = await fetchItems();
-    await renderItem(items);
+    console.log(items);
+    for (let item of items) {
+        const li = await renderItem(item);
+        console.log(li);
+        shoppingList.append(li);}
 
 });
 
@@ -15,10 +24,6 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-const create = document.getElementById('create');
-const quantity = document.getElementById('quantity');
-const item = document.getElementById('item');
-const shoppingList = document.getElementById('shoppingList');
 
 create.addEventListener('submit', async (e)=> {
     shoppingList.innerHTML = '';
--- render-utils.js
+++ render-utils.js
@@ -1,27 +1,15 @@
-import { updateBought } from './fetch-utils.js';
+// import { updateBought } from './fetch-utils.js';
 
-export async function renderItem(items) {
+export async function renderItem(item) {
     // get items from a fetchItems function
-    const shoppingList = document.getElementById('shoppingList');
-
-    for (let item of items) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${item.quantity} ${item.item}`;
-        console.log(`${item.quantity} ${item.item}, ${item.bought}`);
-        shoppingList.append(listItem);
-        if (`${item.bought}` === 'true') {
-            listItem.classList.add('bought');
-        }
-        listItem.addEventListener('click', async ()=> {
-            listItem.classList.add('bought');
-            console.log('clicked');
-            //run a function in fetch.utils that updates a row using the item's id.
-            await updateBought(item.id);
-        });
-        
-    }   
-    return shoppingList;
-}
+    console.log(item.quantity, item.item);
+    const listItem = document.createElement('li');
+    listItem.textContent = `${item.quantity} ${item.item}`;
+    if (`${item.bought}` === 'true') {
+        listItem.classList.add('bought');
+    }
+    return listItem;
+}   
 
 // this function should return just an li, other functions will be pushed to other page.js.
 // appending to shoppingList
@@ -29,3 +17,11 @@ export async function renderItem(items) {
 //      shoppingList.append(listItem)
 // adding eventListener (same const);
 //      listItem.addEventListener
+
+
+// listItem.addEventListener('click', async ()=> {
+//     listItem.classList.add('bought');
+//     console.log('clicked');
+//     //run a function in fetch.utils that updates a row using the item's id.
+//     await updateBought(item.id);
+// });
